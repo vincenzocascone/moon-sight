b0d58e1862bab42a84d63a576ba82c50be76d5c6
Vincenzo117 <94491363+Vincenzo117@users.noreply.github.com>
2023-04-23 16:56:17 +0200

    Deployment fix

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Moon Sight
 MoonSight is a website featuring a 3D model of the moon, displaying its current phase, name, and date. Users can easily change the date to explore past or future phases, with the model and phase name updating accordingly.
+
+## Tasks 
+
+- Check Orientation Device Sensor 
+- Refactor World Classes
+- Improve World Classes
--- src/experience/sources.ts
+++ src/experience/sources.ts
@@ -4,21 +4,21 @@ export default [
   {
     name: "moonColorTexture",
     type: "texture",
-    path: "/assets/textures/moon/moon-color.jpg",
+    path: "src/assets/textures/moon/moon-color.jpg",
   },
   {
     name: "moonBumpTexture",
     type: "texture",
-    path: "/assets/textures/moon/moon-bump.jpg",
+    path: "src/assets/textures/moon/moon-bump.jpg",
   },
   {
     name: "text",
     type: "font",
-    path: "/assets/fonts/play-regular.json",
+    path: "src/assets/fonts/play-regular.json",
   },
   {
     name: "textMatcap",
     type: "texture",
-    path: "/assets/matcaps/text-matcap.png",
+    path: "src/assets/matcaps/text-matcap.png",
   },
 ] as Source[];
