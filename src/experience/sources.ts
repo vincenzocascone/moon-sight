69aacb1763174aa11caae500c1e064cd7f580d8a
Vincenzo117 <94491363+Vincenzo117@users.noreply.github.com>
2023-04-30 09:26:36 +0200

    Test

--- src/experience/sources.ts
+++ src/experience/sources.ts
@@ -4,21 +4,21 @@ export default [
   {
     name: "moonColorTexture",
     type: "texture",
-    path: "/assets/textures/moon/moon-color.jpg",
+    path: "/textures/moon/moon-color.jpg",
   },
   {
     name: "moonBumpTexture",
     type: "texture",
-    path: "/assets/textures/moon/moon-bump.jpg",
+    path: "/textures/moon/moon-bump.jpg",
   },
   {
     name: "text",
     type: "font",
-    path: "/assets/fonts/play-regular.json",
+    path: "/fonts/play-regular.json",
   },
   {
     name: "textMatcap",
     type: "texture",
-    path: "/assets/matcaps/text-matcap.png",
+    path: "/matcaps/text-matcap.png",
   },
 ] as Source[];
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import "./style.scss";
-import { inject } from "@vercel/analytics";
+// import { inject } from "@vercel/analytics";
 
 import Experience from "./experience/Experience";
 
-inject();
+// inject analytics only if in production
+if (process.env.NODE_ENV === "production") {
+ // inject();
+}
+console.log(process.env.NODE_ENV)
 
 Experience.getInstance();
