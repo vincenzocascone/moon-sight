250470196b5e56913a4be96d704f63eb3f8c9666
Vincenzo117 <94491363+Vincenzo117@users.noreply.github.com>
2023-05-12 22:43:39 +0200

    vercel analytics removed

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,4 @@ import "./style.scss";
 
 import Experience from "./experience/Experience";
 
-if (process.env.NODE_ENV === "production") {
-    import("@vercel/analytics")
-        .then(({ inject }) => {
-            inject();
-        })
-        .catch((error) => console.error("Failed to load vercel analytics:", error));
-}
-
 Experience.getInstance();
